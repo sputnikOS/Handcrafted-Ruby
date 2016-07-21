dfd1007c95c1d54d4a53383368837badd6f19de4
sabsowitz <sabsowitz@yahoo.com>
2016-07-21 19:30:01 -0400

    Replaced Curb with HTTParty. HTTParty seems to be faster for the GET request. -Refactored the search method so it no longer depends upon the Shodan gem.

--- search.rb
+++ search.rb
@@ -1,21 +1,21 @@
-require 'shodan'
-require 'curb'
+require 'optparse'
+require 'httparty'
 
 
-api_key = ''
+api_key = 'bnjjf87YSnQGrO4IagIrClllc8LJcWqt'
 base_url = 'https://api.shodan.io/'
 
 # Gets and displays api key information
 def info(api_key, base_url)
-  api_info = 'api-info?key='
-  ip_info = 'tools/myip?key='
-  url = base_url + api_info + api_key
-  userIP = base_url + ip_info + api_key
+  api_url = 'api-info?key='
+  ip_url = 'tools/myip?key='
+  url = base_url + api_url + api_key
+  userIP = base_url + ip_url + api_key
   begin
-    a = Curl::Easy.perform(userIP)
-    puts "User IP Address is #{a.body_str}"
-    c = Curl::Easy.perform(url)
-    results = JSON.parse(c.body_str)
+    a = HTTParty.get(userIP)
+    puts "User IP Address is #{a.body}"
+    c = HTTParty.get(url)
+    results = JSON.parse(c.body)
     puts "Shodan API Key Confirmed" + "!"
     puts "API Key" + ": #{api_key}"
     puts "Plan Type" + ": #{results['plan']}"
@@ -26,24 +26,27 @@ def info(api_key, base_url)
   end
 end
 
-def search(api_key)
-  puts "=================="
-  puts "Enter search input:"
-  input = gets.chomp
-  api = Shodan::Shodan.new(api_key)
-  puts "Searching results for #{input}"
-  result = api.search(input)
-  result['matches'].each{ |host|
-    puts host['org']
-    puts host['hostnames']
-    puts host['ip_str']
-    puts host['port']
-    puts host['transport']
-    # puts host['data']
-    puts "===================== \n"
-  }
+def search(api_key, base_url)
+  begin
+    puts "==================="
+    puts "Enter search input:"
+    input = gets.chomp
+    query = base_url + 'shodan/host/search?key=' + api_key + '&query=' + input
+    search = HTTParty.get(query)
+    puts "Searching results for #{input}"
+    results = JSON.parse(search.body)
+    results['matches'].each{ |host|
+      puts host['org']
+      puts host['hostnames']
+      puts host['ip_str']
+      puts host['port']
+      puts host['transport']
+      # puts host['data']
+      puts "===================== \n"
+    }
+  end
 end
 
 info(api_key, base_url)
-search(api_key)
+search(api_key, base_url)
 
